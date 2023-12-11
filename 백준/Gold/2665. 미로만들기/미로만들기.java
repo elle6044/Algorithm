8594dec3b7f2fda890e9904eedfc4d5d0c590379
elle6044 <123047819+elle6044@users.noreply.github.com>
2023-12-11 10:08:50 +0900

    [Gold IV] Title: 미로만들기, Time: 84 ms, Memory: 11844 KB -BaekjoonHub

--- 백준/Gold/2665. 미로만들기/미로만들기.java
+++ 백준/Gold/2665. 미로만들기/미로만들기.java
@@ -1,80 +1,66 @@
 import java.io.*;
-import java.util.ArrayDeque;
-import java.util.PriorityQueue;
-import java.util.Queue;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class Main {
-    static BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
-    static BufferedWriter bw=new BufferedWriter(new OutputStreamWriter(System.out));
-    static StringTokenizer st;
-
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
     static int N;
     static int[][] map;
-    static boolean[][][] v;
+    static boolean[][] visited;
+    static int[] dr = {1, -1, 0, 0};
+    static int[] dc = {0, 0, 1, -1};
 
-    static int[] dr={1,-1,0,0};
-    static int[] dc={0,0,1,-1};
-    static class Point implements Comparable<Point>{
-        int r,c,black;
-        public Point(int r,int c,int black){
-            this.r=r;
-            this.c=c;
-            this.black=black;
-        }
-
-        @Override
-        public int compareTo(Point o) {
-            return this.black-o.black;
+    static class Point {
+        int r, c, black;
+        Point(int r, int c, int black) {
+            this.r = r;
+            this.c = c;
+            this.black = black;
         }
     }
 
-    static PriorityQueue<Point> q=new PriorityQueue<>();
-    static int minCnt=Integer.MAX_VALUE;
-
     public static void main(String[] args) throws IOException {
-        N=Integer.parseInt(br.readLine());
-        map=new int[N][N];
-        v=new boolean[N*N][N][N];
+        N = Integer.parseInt(br.readLine());
+        map = new int[N][N];
+        visited = new boolean[N][N];
 
-        for(int i=0;i<N;i++){
-            String word=br.readLine();
-            for(int j=0;j<N;j++){
-                map[i][j]=Character.getNumericValue(word.charAt(j));
+        for (int i = 0; i < N; i++) {
+            String line = br.readLine();
+            for (int j = 0; j < N; j++) {
+                map[i][j] = line.charAt(j) - '0';
             }
         }
 
-        bfs();
+        int minCnt = bfs();
 
-        bw.write(minCnt+"");
+        bw.write(minCnt + "");
         bw.close();
     }
 
-    private static void bfs() {
-        q.offer(new Point(0,0,0));
-        v[0][0][0]=true;
+    private static int bfs() {
+        Deque<Point> deque = new ArrayDeque<>();
+        deque.offer(new Point(0, 0, 0));
+        visited[0][0] = true;
 
-        while(!q.isEmpty()){
-            Point p=q.poll();
-            if(p.r==N-1&&p.c==N-1&&p.black<minCnt){
-                minCnt=p.black;
-                return;
+        while (!deque.isEmpty()) {
+            Point p = deque.poll();
+            if (p.r == N - 1 && p.c == N - 1) {
+                return p.black;
             }
 
-            for(int d=0;d<4;d++){
-                int nr=p.r+dr[d];
-                int nc=p.c+dc[d];
-                if(nr>=0&&nr<N&&nc>=0&&nc<N){
-                    if(map[nr][nc]==1&&!v[p.black][nr][nc]){
-                        q.offer(new Point(nr,nc, p.black));
-                        v[p.black][nr][nc]=true;
+            for (int d = 0; d < 4; d++) {
+                int nr = p.r + dr[d];
+                int nc = p.c + dc[d];
+                if (nr >= 0 && nr < N && nc >= 0 && nc < N && !visited[nr][nc]) {
+                    if (map[nr][nc] == 1) {
+                        deque.offerFirst(new Point(nr, nc, p.black));
+                    } else {
+                        deque.offerLast(new Point(nr, nc, p.black + 1));
                     }
-                    if(map[nr][nc]==0&&p.black+1<N*N&&!v[p.black+1][nr][nc]){
-                        q.offer(new Point(nr,nc,p.black+1));
-                        v[p.black+1][nr][nc]=true;
-                    }
+                    visited[nr][nc] = true;
                 }
             }
         }
+        return Integer.MAX_VALUE;
     }
 }
