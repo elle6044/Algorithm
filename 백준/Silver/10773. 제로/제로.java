9af45a6f9009593da759125cf9ad5e8b190f1de0
elle6044 <123047819+elle6044@users.noreply.github.com>
2023-12-14 20:37:48 +0900

    [Silver IV] Title: 제로, Time: 136 ms, Memory: 13548 KB -BaekjoonHub

--- 백준/Silver/10773. 제로/제로.java
+++ 백준/Silver/10773. 제로/제로.java
@@ -2,17 +2,16 @@ import java.io.*;
 import java.util.Stack;
 
 public class Main {
-    static BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
     static BufferedWriter bw=new BufferedWriter(new OutputStreamWriter(System.out));
 
     static int K;
     static Stack<Integer> stack=new Stack<>();
 
-    public static void main(String[] args) throws IOException {
-        K=Integer.parseInt(br.readLine());
+    public static void main(String[] args) throws Exception {
+        K=nextInt();
         int sum=0;
         for(int i=0;i<K;i++){
-            int input=Integer.parseInt(br.readLine());
+            int input=nextInt();
             if(input!=0){
                 stack.push(input);
                 sum+=input;
@@ -24,5 +23,19 @@ public class Main {
 
         bw.write(sum+"");
         bw.close();
+    }
+
+    static int nextInt() throws Exception {
+        int sign = 0;
+        int n = 0;
+        int c;
+        while ((c = System.in.read()) <= 32);
+        if(c == 45) {
+            sign = 1;
+            c = System.in.read();
+        }
+        do n = (n << 3) + (n << 1) + (c & 15);
+        while ((c = System.in.read()) > 47 && c < 58);
+        return sign == 0 ? n : ~n + 1;
     }
 }
