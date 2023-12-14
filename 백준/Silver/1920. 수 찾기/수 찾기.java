564fd5ed26643580516f978cad6e47485b83f1c3
elle6044 <123047819+elle6044@users.noreply.github.com>
2023-12-14 20:18:42 +0900

    [Silver IV] Title: 수 찾기, Time: 336 ms, Memory: 22980 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.java
+++ 백준/Silver/1920. 수 찾기/수 찾기.java
@@ -7,25 +7,38 @@ public class Main {
     static BufferedWriter bw=new BufferedWriter(new OutputStreamWriter(System.out));
     static StringTokenizer st;
 
-    public static void main(String[] args) throws IOException {
-        int N=Integer.parseInt(br.readLine());
-        HashSet<String> hashSet=new HashSet<>();
-        st=new StringTokenizer(br.readLine());
-        while(st.hasMoreTokens()){
-            hashSet.add(st.nextToken());
+    public static void main(String[] args) throws Exception {
+        int N=nextInt();
+        HashSet<Integer> hashSet=new HashSet<>(N);
+//        st=new StringTokenizer(br.readLine());
+        for(int i=0;i<N;i++){
+            hashSet.add(nextInt());
         }
 
-        int M=Integer.parseInt(br.readLine());
-        st=new StringTokenizer(br.readLine());
-        while(st.hasMoreTokens()){
-            if(hashSet.contains(st.nextToken())){
+        int M=nextInt();
+        for(int i=0;i<M;i++){
+            if(hashSet.contains(nextInt())){
                 bw.write("1\n");
             }
             else{
                 bw.write("0\n");
             }
         }
-
         bw.close();
+    }
+
+
+    static int nextInt() throws Exception {
+        int sign = 0;
+        int n = 0;
+        int c;
+        while ((c = System.in.read()) <= 32);
+        if(c == 45) {
+            sign = 1;
+            c = System.in.read();
+        }
+        do n = (n << 3) + (n << 1) + (c & 15);
+        while ((c = System.in.read()) > 47 && c < 58);
+        return sign == 0 ? n : ~n + 1;
     }
 }
