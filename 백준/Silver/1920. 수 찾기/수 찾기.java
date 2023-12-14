afb4c7265442859e965ef5c78a4f33471b0b81a7
elle6044 <123047819+elle6044@users.noreply.github.com>
2023-12-14 20:08:46 +0900

    [Silver IV] Title: 수 찾기, Time: 464 ms, Memory: 56208 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.java
+++ 백준/Silver/1920. 수 찾기/수 찾기.java
@@ -9,16 +9,16 @@ public class Main {
 
     public static void main(String[] args) throws IOException {
         int N=Integer.parseInt(br.readLine());
-        HashSet<Integer> hashSet=new HashSet<>();
+        HashSet<String> hashSet=new HashSet<>();
         st=new StringTokenizer(br.readLine());
         while(st.hasMoreTokens()){
-            hashSet.add(Integer.parseInt(st.nextToken()));
+            hashSet.add(st.nextToken());
         }
 
         int M=Integer.parseInt(br.readLine());
         st=new StringTokenizer(br.readLine());
         while(st.hasMoreTokens()){
-            if(hashSet.contains(Integer.parseInt(st.nextToken()))){
+            if(hashSet.contains(st.nextToken())){
                 bw.write("1\n");
             }
             else{
