c0a3132438fbb4453b5d2f943c59a171db50156b
elle6044 <123047819+elle6044@users.noreply.github.com>
2023-12-16 17:40:34 +0900

    [Silver IV] Title: 요세푸스 문제, Time: 160 ms, Memory: 12928 KB -BaekjoonHub

--- 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.java
+++ 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.java
@@ -8,44 +8,46 @@ public class Main {
     static BufferedWriter bw=new BufferedWriter(new OutputStreamWriter(System.out));
     static int N,K;
 
-    static class Node {
-        int data;
-        Node next;
-
-        public Node(int data) {
-            this.data = data;
-            this.next = null;
+    static class Point{
+        int num;
+        Point next;
+        public Point(int num){
+            this.num=num;
+            this.next=null;
         }
     }
-
 
     public static void main(String[] args) throws Exception {
         N=nextInt();
         K=nextInt();
 
-        Node head = new Node(1);
-        Node tail = head;
-        
-        for (int i = 2; i <= N; i++) {
-            tail.next = new Node(i);
-            tail = tail.next;
+        Point head=new Point(1);
+        Point tail=head;
+        for(int i=2;i<=N;i++){
+            tail.next=new Point(i);
+            tail=tail.next;
         }
-        tail.next = head;
+        tail.next=head;
 
         bw.write("<");
-
-        Node current = head;
-        while (N-- > 0) {
-            for (int i = 1; i < K; i++) {
-                current = current.next;
+        Point current=head;
+        while(N-->0){
+            for(int i=0;i<K-1;i++){
+                current=current.next;
             }
-            bw.write(current.data+"");
-            if (N > 0) bw.write(", ");
-            
-            current.data = current.next.data;
-            current.next = current.next.next;
+
+
+            if(N!=0){
+                bw.write(current.num+", ");
+            }
+            else{
+                bw.write(current.num+">");
+            }
+
+            current.num=current.next.num;
+            current.next=current.next.next;
         }
-        bw.write(">");
+        
         bw.close();
     }
 
