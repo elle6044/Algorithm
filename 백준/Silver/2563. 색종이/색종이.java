5cb5a7eb67a677b7108e8ae3fd8e81142e91c7d7
elle6044 <123047819+elle6044@users.noreply.github.com>
2024-01-14 14:22:14 +0900

    [Silver V] Title: 색종이, Time: 80 ms, Memory: 11588 KB -BaekjoonHub

--- 백준/Silver/2563. 색종이/색종이.java
+++ 백준/Silver/2563. 색종이/색종이.java
@@ -8,15 +8,14 @@ public class Main {
     static BufferedWriter bw=new BufferedWriter(new OutputStreamWriter(System.out));
     static StringTokenizer st;
 
-    public static void main(String[] args) throws IOException {
-        int N=Integer.parseInt(br.readLine());
+    public static void main(String[] args) throws Exception {
+        int N=nextInt();
         boolean[][] map=new boolean[101][101];
 
         int cnt=0;
         for(int i=0;i<N;i++){
-            st=new StringTokenizer(br.readLine());
-            int sx=Integer.parseInt(st.nextToken());
-            int sy=Integer.parseInt(st.nextToken());
+            int sx=nextInt();
+            int sy=nextInt();
             int ex=sx+10;
             int ey=sy+10;
 
@@ -32,6 +31,21 @@ public class Main {
         }
         bw.write(cnt+"");
         bw.close();
+    }
+
+
+    static int nextInt() throws Exception {
+        int sign = 0;
+        int n = 0;
+        int c;
+        while ((c = System.in.read()) <= 32);
+        if(c == 45) {
+            sign = 1;
+            c = System.in.read();
+        }
+        do n = (n << 3) + (n << 1) + (c & 15);
+        while ((c = System.in.read()) > 47 && c < 58);
+        return sign == 0 ? n : ~n + 1;
     }
 
 
