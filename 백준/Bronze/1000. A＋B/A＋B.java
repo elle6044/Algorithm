b73bc77b684a585bbd10b88af5a8fa663e73abc1
elle6044 <123047819+elle6044@users.noreply.github.com>
2024-02-03 23:32:31 +0900

    [Bronze V] Title: A+B, Time: 76 ms, Memory: 11604 KB -BaekjoonHub

---
+++ 백준/Bronze/1000. A＋B/A＋B.java
@@ -0,0 +1,12 @@
+import java.io.*;
+import java.util.StringTokenizer;
+public class Main {
+    static BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+    static BufferedWriter bw=new BufferedWriter(new OutputStreamWriter(System.out));
+    static StringTokenizer st;
+    public static void main(String[] args) throws IOException {
+        st=new StringTokenizer(br.readLine());
+        bw.write((Integer.parseInt(st.nextToken())+Integer.parseInt(st.nextToken()))+"");
+        bw.close();
+    }
+}
