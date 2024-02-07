628ffcfdb39923e76e1030bf7a09be7752fd153e
elle6044 <123047819+elle6044@users.noreply.github.com>
2024-02-08 08:43:44 +0900

    [Silver III] Title: 1로 만들기, Time: 100 ms, Memory: 15600 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.java
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.java
@@ -9,9 +9,7 @@ public class Main {
     public static void main(String[] args) throws IOException {
         int N=Integer.parseInt(br.readLine());
         array=new int[N+1];
-        for(int i=1;i<=N/3;i++){
-            array[i*3]=i;
-        }
+
 
         for(int i=2;i<=N;i++){
             array[i]=array[i-1]+1;
