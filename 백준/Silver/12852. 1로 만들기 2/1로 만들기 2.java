44332e97c9c91aa38aa65c3305a81dd0f02077ca
elle6044 <123047819+elle6044@users.noreply.github.com>
2024-02-11 13:50:32 +0900

    [Silver I] Title: 1로 만들기 2, Time: 104 ms, Memory: 19532 KB -BaekjoonHub

--- 백준/Silver/12852. 1로 만들기 2/1로 만들기 2.java
+++ 백준/Silver/12852. 1로 만들기 2/1로 만들기 2.java
@@ -9,39 +9,34 @@ public class Main {
     public static void main(String[] args) throws IOException {
         int n=Integer.parseInt(br.readLine());
         int[] dp=new int[n+1];
-        Stack[] stacks=new Stack[n+1];
-        Stack<Integer> oneStack=new Stack<>();
-        oneStack.push(1);
-        stacks[1]=oneStack;
+        int[] prev=new int[n+1];
+
         for(int i=2;i<=n;i++){
             dp[i]=dp[i-1]+1;
-            Stack<Integer> stack=new Stack<>();
-            stack.addAll(stacks[i-1]);
-            stack.push(i);
+            prev[i]=i-1;
 
             if(i%3==0){
                 if(dp[i/3]+1<dp[i]){
                     dp[i]=dp[i/3]+1;
-                    stack.clear();
-                    stack.addAll(stacks[i/3]);
-                    stack.push(i);
+                    prev[i]=i/3;
                 }
             }
             if(i%2==0){
                 if(dp[i/2]+1<dp[i]){
                     dp[i]=dp[i/2]+1;
-                    stack.clear();
-                    stack.addAll(stacks[i/2]);
-                    stack.push(i);
+                    prev[i]=i/2;
                 }
             }
-            stacks[i]=stack;
         }
 
         bw.write(dp[n]+"\n");
-        while(!stacks[n].isEmpty()){
-            bw.write(stacks[n].pop()+" ");
+        bw.write(n+" ");
+        while(n>1){
+            int num=prev[n];
+            bw.write(num+" ");
+            n=num;
         }
+
         bw.close();
     }
 }
