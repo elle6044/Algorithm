8efd9abbe82d384dd033587ac06b3a7ebe6a10ed
elle6044 <123047819+elle6044@users.noreply.github.com>
2024-02-12 17:41:13 +0900

    [Silver I] Title: 포도주 시식, Time: 232 ms, Memory: 12820 KB -BaekjoonHub

---
+++ 백준/Silver/2156. 포도주 시식/포도주 시식.java
@@ -0,0 +1,53 @@
+import java.io.BufferedWriter;
+import java.io.OutputStreamWriter;
+import java.util.Arrays;
+
+public class Main {
+    static BufferedWriter bw=new BufferedWriter(new OutputStreamWriter(System.out));
+
+    public static void main(String[] args) throws Exception {
+        int n=nextInt();
+        int[] array=new int[n+1];
+        for(int i=0;i<n;i++){
+            array[i]=nextInt();
+        }
+        int[] dp=new int[n+1];
+        dp[0]=array[0];
+        dp[1]=array[1]+array[0];
+        if(n>=2){
+            dp[2]=Math.max(array[0],array[1])+array[2];
+            for(int i=3;i<n;i++){
+                int prevMax=0;
+                for(int j=0;j<i-1;j++){
+                    prevMax=Math.max(prevMax,dp[j]);
+                }
+                int prevMax2=0;
+                for(int j=0;j<i-2;j++){
+                    prevMax2=Math.max(prevMax2,dp[j]+array[i-1]);
+                }
+                dp[i]=Math.max(prevMax,prevMax2)+array[i];
+            }
+        }
+
+        int max=0;
+        for (int i : dp) {
+            max=Math.max(max,i);
+        }
+        bw.write(max+"");
+        bw.close();
+    }
+    static int nextInt() throws Exception {
+        int sign = 0;
+        int n = 0;
+        int c;
+        while ((c = System.in.read()) <= 32);
+        if(c == 45) {
+            sign = 1;
+            c = System.in.read();
+        }
+        do n = (n << 3) + (n << 1) + (c & 15);
+        while ((c = System.in.read()) > 47 && c < 58);
+        return sign == 0 ? n : ~n + 1;
+    }
+
+}
