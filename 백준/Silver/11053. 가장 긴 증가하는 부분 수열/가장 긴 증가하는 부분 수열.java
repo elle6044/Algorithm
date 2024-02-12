d8597202c14c366a31c52acea44239ed5b5cd9aa
elle6044 <123047819+elle6044@users.noreply.github.com>
2024-02-12 15:31:46 +0900

    [Silver II] Title: 가장 긴 증가하는 부분 수열, Time: 92 ms, Memory: 12272 KB -BaekjoonHub

---
+++ 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.java
@@ -0,0 +1,51 @@
+import java.io.BufferedWriter;
+import java.io.OutputStreamWriter;
+import java.util.Arrays;
+
+public class Main {
+    static BufferedWriter bw=new BufferedWriter(new OutputStreamWriter(System.out));
+
+    public static void main(String[] args) throws Exception {
+        int N=nextInt();
+        int[] array=new int[N];
+        int[] dp=new int[N];
+        for(int i=0;i<N;i++){
+            array[i]=nextInt();
+        }
+        Arrays.fill(dp,1);
+
+        for(int i=0;i<N;i++){
+            for(int j=0;j<i;j++){
+                if(array[i]>array[j]){
+                    dp[i]=Math.max(dp[i],dp[j]+1);
+                }
+            }
+        }
+
+        int max=0;
+        for (int i : dp) {
+            max=Math.max(max,i);
+        }
+        bw.write(max+"");
+        bw.close();
+
+
+
+
+
+    }
+
+    static int nextInt() throws Exception {
+        int sign = 0;
+        int n = 0;
+        int c;
+        while ((c = System.in.read()) <= 32);
+        if(c == 45) {
+            sign = 1;
+            c = System.in.read();
+        }
+        do n = (n << 3) + (n << 1) + (c & 15);
+        while ((c = System.in.read()) > 47 && c < 58);
+        return sign == 0 ? n : ~n + 1;
+    }
+}
