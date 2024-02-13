7db4ae7453419119dcf70717aca8022e23faf411
elle6044 <123047819+elle6044@users.noreply.github.com>
2024-02-13 17:40:35 +0900

    [Silver II] Title: 1, 2, 3 더하기 3, Time: 108 ms, Memory: 19520 KB -BaekjoonHub

--- 백준/Silver/15988. 1， 2， 3 더하기 3/1， 2， 3 더하기 3.java
+++ 백준/Silver/15988. 1， 2， 3 더하기 3/1， 2， 3 더하기 3.java
@@ -5,16 +5,16 @@ public class Main {
     static BufferedWriter bw=new BufferedWriter(new OutputStreamWriter(System.out));
 
     public static void main(String[] args) throws Exception {
+        long[] dp=new long[1000001];
+        dp[1]=1;
+        dp[2]=2;
+        dp[3]=4;
+        for(int i=4;i<=1000000;i++){
+            dp[i]=(dp[i-1]+dp[i-2]+dp[i-3])%1000000009;
+        }
         int T=nextInt();
         for(int t=0;t<T;t++){
             int n=nextInt();
-            long[] dp=new long[n+3];
-            dp[1]=1;
-            dp[2]=2;
-            dp[3]=4;
-            for(int i=4;i<=n;i++){
-                dp[i]=(dp[i-1]+dp[i-2]+dp[i-3])%1000000009;
-            }
             bw.write(dp[n]+"\n");
         }
         bw.close();
