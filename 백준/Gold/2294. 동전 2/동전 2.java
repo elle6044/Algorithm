fab4ea243ecf9f523d13e33d7a4f3b93fd86d5f2
elle6044 <123047819+elle6044@users.noreply.github.com>
2024-02-14 13:54:52 +0900

    [Gold V] Title: 동전 2, Time: 576 ms, Memory: 13452 KB -BaekjoonHub

---
+++ 백준/Gold/2294. 동전 2/동전 2.java
@@ -0,0 +1,70 @@
+import java.io.BufferedWriter;
+import java.io.OutputStreamWriter;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.Set;
+import java.util.TreeSet;
+
+public class Main {
+    static BufferedWriter bw=new BufferedWriter(new OutputStreamWriter(System.out));
+
+    public static void main(String[] args) throws Exception {
+        int n=nextInt();
+        int k=nextInt();
+        Set<Integer> set=new TreeSet<>(Comparator.reverseOrder());
+        for(int i=0;i<n;i++){
+            set.add(nextInt());
+        }
+
+
+        int[] dp=new int[k+1];
+        for(int i=1;i<=k;i++){
+            for (int coin : set) {
+                int coinCnt=i/coin;
+                for(int j=1;j<=coinCnt;j++){
+                    if(dp[i]==0){
+                        if(i-coin*j!=0&&dp[i-coin*j]==0){
+
+                        }
+                        else{
+                            dp[i]=dp[i-coin*j]+j;
+                        }
+
+                    }
+                    else{
+                        if(i-coin*j!=0&&dp[i-coin*j]==0){
+
+                        }
+                        else{
+                            dp[i]=Math.min(dp[i],
+                                    dp[i-coin*j]+j);
+                        }
+                    }
+                }
+            }
+        }
+
+        if(dp[k]==0){
+            bw.write("-1");
+        }
+        else{
+            bw.write(dp[k]+"");
+        }
+        bw.close();
+
+    }
+    static int nextInt() throws Exception {
+        int sign = 0;
+        int n = 0;
+        int c;
+        while ((c = System.in.read()) <= 32);
+        if(c == 45) {
+            sign = 1;
+            c = System.in.read();
+        }
+        do n = (n << 3) + (n << 1) + (c & 15);
+        while ((c = System.in.read()) > 47 && c < 58);
+        return sign == 0 ? n : ~n + 1;
+    }
+
+}
