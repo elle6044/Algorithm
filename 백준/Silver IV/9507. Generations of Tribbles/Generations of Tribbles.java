3f8b7226ec1cc447a7cb61d870676664c0e57484
elle6044 <123047819+elle6044@users.noreply.github.com>
2024-02-14 19:02:26 +0900

    [Silver IV] Title: Generations of Tribbles, Time: 72 ms, Memory: 11340 KB -BaekjoonHub

--- 백준/Silver IV/9507. Generations of Tribbles/Generations of Tribbles.java
+++ 백준/Silver IV/9507. Generations of Tribbles/Generations of Tribbles.java
@@ -11,11 +11,11 @@ public class Main {
         dp[1]=1;
         dp[2]=2;
         dp[3]=4;
+        for(int i=4;i<68;i++){
+            dp[i]=dp[i-1]+dp[i-2]+dp[i-3]+dp[i-4];
+        }
         for(int t=0;t<T;t++){
             int n=nextInt();
-            for(int i=4;i<=n;i++){
-                dp[i]=dp[i-1]+dp[i-2]+dp[i-3]+dp[i-4];
-            }
             bw.write(dp[n]+"\n");
         }
         bw.close();
