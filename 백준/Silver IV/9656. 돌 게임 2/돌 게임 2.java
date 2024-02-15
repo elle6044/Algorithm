627c6659c3a3d99c72d4081a67cf704543ad687a
elle6044 <123047819+elle6044@users.noreply.github.com>
2024-02-15 13:40:13 +0900

    [Silver IV] Title: 돌 게임 2, Time: 76 ms, Memory: 11612 KB -BaekjoonHub

---
+++ 백준/Silver IV/9656. 돌 게임 2/돌 게임 2.java
@@ -0,0 +1,12 @@
+import java.io.*;
+
+public class Main {
+    static BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+    static BufferedWriter bw=new BufferedWriter(new OutputStreamWriter(System.out));
+
+    public static void main(String[] args) throws IOException {
+        int n=Integer.parseInt(br.readLine());
+        bw.write(n%2==0?"SK":"CY");
+        bw.close();
+    }
+}
