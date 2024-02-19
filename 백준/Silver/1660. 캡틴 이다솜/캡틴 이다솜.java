a21f9fa67ea71bedc0e8bd080124307a07faa9bb
elle6044 <123047819+elle6044@users.noreply.github.com>
2024-02-19 11:37:12 +0900

    [Silver I] Title: 캡틴 이다솜, Time: 160 ms, Memory: 13728 KB -BaekjoonHub

---
+++ 백준/Silver/1660. 캡틴 이다솜/캡틴 이다솜.java
@@ -0,0 +1,46 @@
+import java.io.*;
+import java.util.Arrays;
+
+public class Main {
+    static BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+    static BufferedWriter bw=new BufferedWriter(new OutputStreamWriter(System.out));
+
+    public static void main(String[] args) throws IOException {
+
+        int[] dp=new int[122];
+        dp[1]=1;
+        for(int i=2;i<=121;i++){
+            dp[i]=dp[i-1]+(i%2==0?(i+1)*(i/2):(i+1)/2*i);
+        }
+
+        int n=Integer.parseInt(br.readLine());
+        int[] dp2=new int[n+1];
+        for(int i=1;i<=n;i++){
+            dp2[i]=i;
+        }
+
+
+//        for(int k=2;true;k++){
+//            int num=dp[k];
+//            if(num>n) break;
+//            for(int i=num;i<=n;i++){
+//                int cnt=i/num;
+//                for(int j=1;j<=cnt;j++){
+//                    if (j>=dp2[i]) break;
+//                    dp2[i]=Math.min(dp2[i],cnt+dp2[i-cnt*num]);
+//                }
+//            }
+//        }
+
+        for(int k=2;true;k++){
+            int num=dp[k];
+            if(num>n) break;
+            for(int i=num;i<=n;i++){
+                dp2[i]=Math.min(dp2[i],1+dp2[i-num]);
+            }
+        }
+
+        bw.write(dp2[n]+"");
+        bw.close();
+    }
+}
