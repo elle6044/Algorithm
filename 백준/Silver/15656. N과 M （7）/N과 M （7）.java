e4db469dee69d199b6fb312d2e5516a6e831e640
elle6044 <123047819+elle6044@users.noreply.github.com>
2024-02-21 14:36:14 +0900

    [Silver III] Title: N과 M (7), Time: 624 ms, Memory: 220540 KB -BaekjoonHub

---
+++ 백준/Silver/15656. N과 M （7）/N과 M （7）.java
@@ -0,0 +1,58 @@
+import java.io.BufferedWriter;
+import java.io.OutputStreamWriter;
+import java.util.Arrays;
+
+public class Main {
+    static BufferedWriter bw=new BufferedWriter(new OutputStreamWriter(System.out));
+
+    static int N,M;
+    static int[] num;
+    static int[] array;
+
+    public static void main(String[] args) throws Exception {
+        N=nextInt();
+        M=nextInt();
+        num=new int[N];
+        for(int i=0;i<N;i++){
+            num[i]=nextInt();
+        }
+        Arrays.sort(num);
+
+        array=new int[M];
+        back(0);
+
+        bw.write(sb.toString());
+        bw.close();
+    }
+
+    static StringBuilder sb=new StringBuilder();
+    private static void back(int cnt) {
+        if(cnt==M){
+            for (int i : array) {
+                sb.append(i).append(" ");
+            }
+            sb.append("\n");
+            return;
+        }
+
+        for(int i=0;i<N;i++){
+            array[cnt]=num[i];
+            back(cnt+1);
+        }
+    }
+
+    static int nextInt() throws Exception {
+        int sign = 0;
+        int n = 0;
+        int c;
+        while ((c = System.in.read()) <= 32);
+        if(c == 45) {
+            sign = 1;
+            c = System.in.read();
+        }
+        do n = (n << 3) + (n << 1) + (c & 15);
+        while ((c = System.in.read()) > 47 && c < 58);
+        return sign == 0 ? n : ~n + 1;
+    }
+
+}
