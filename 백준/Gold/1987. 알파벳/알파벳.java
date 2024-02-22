55820cbad58b95deec58fa87e6b1c03ac8ce1aca
elle6044 <123047819+elle6044@users.noreply.github.com>
2024-02-23 07:38:56 +0900

    [Gold IV] Title: 알파벳, Time: 916 ms, Memory: 12348 KB -BaekjoonHub

--- 백준/Gold/1987. 알파벳/알파벳.java
+++ 백준/Gold/1987. 알파벳/알파벳.java
@@ -10,28 +10,28 @@ public class Main {
     static StringTokenizer st;
 
     static int R,C;
-    static char[][] map;
+    static int[][] map;
 
 
     static int[] dr={1,-1,0,0};
     static int[] dc={0,0,1,-1};
 
-    static Set<Character> set=new HashSet<>();
+    static boolean[] words=new boolean[26];
 
     public static void main(String[] args) throws IOException {
         st=new StringTokenizer(br.readLine());
         R=Integer.parseInt(st.nextToken());
         C=Integer.parseInt(st.nextToken());
-        map=new char[R][C];
+        map=new int[R][C];
         for(int i=0;i<R;i++){
             String input=br.readLine();
             for(int j=0;j<C;j++){
-                map[i][j]=input.charAt(j);
+                map[i][j]=input.charAt(j)-65;
             }
         }
+        words[map[0][0]]=true;
 
-        set.add(map[0][0]);
-        back(0,0,0);
+        back(0,0, 1);
 
         bw.write(answer+"");
         bw.close();
@@ -44,12 +44,12 @@ public class Main {
             int nr=r+dr[d];
             int nc=c+dc[d];
             if(nr>=0&&nr<R&&nc>=0&&nc<C){
-                char next=map[nr][nc];
-                if(!set.contains(next)){
-                    set.add(next);
-                    answer= Math.max(answer,set.size());
+                int next=map[nr][nc];
+                if(!words[next]){
+                    words[next]=true;
+                    answer=Math.max(answer,cnt+1);
                     back(nr,nc,cnt+1);
-                    set.remove(next);
+                    words[next]=false;
                 }
             }
         }
