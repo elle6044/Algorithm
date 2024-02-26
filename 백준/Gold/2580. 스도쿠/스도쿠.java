d630d7667db2b9758b935aac3e6d116449609d83
elle6044 <123047819+elle6044@users.noreply.github.com>
2024-02-26 14:48:42 +0900

    [Gold IV] Title: 스도쿠, Time: 368 ms, Memory: 14660 KB -BaekjoonHub

--- 백준/Gold/2580. 스도쿠/스도쿠.java
+++ 백준/Gold/2580. 스도쿠/스도쿠.java
@@ -1,6 +1,7 @@
 import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.OutputStreamWriter;
+import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -8,27 +9,36 @@ public class Main {
     static BufferedWriter bw=new BufferedWriter(new OutputStreamWriter(System.out));
 
     static int[][] map=new int[9][9];
-    static int zeroCnt=0;
+    static ArrayList<Point> list=new ArrayList<>();
 
+    static class Point{
+        int r,c;
+        public Point(int r, int c){
+            this.r=r;
+            this.c=c;
+        }
+    }
 
     public static void main(String[] args) throws Exception {
         for(int i=0;i<9;i++){
             for(int j=0;j<9;j++){
                 int input=nextInt();
                 map[i][j]=input;
-                if(input==0) zeroCnt++;
+                if(input==0) {
+                    list.add(new Point(i,j));
+                }
             }
         }
 
-        back(0,0,0);
+        back(0);
 
         bw.close();
 
     }
 
     static boolean isEnd=false;
-    private static void back(int r, int c, int cnt) throws IOException {
-        if(cnt==zeroCnt){
+    private static void back(int cnt) throws IOException {
+        if(cnt==list.size()){
             for (int[] ints : map) {
                 for (int i : ints) {
                     bw.write(i+" ");
@@ -38,46 +48,17 @@ public class Main {
             isEnd=true;
             return;
         }
-
-        for(int i=r;i<9;i++){
-            for(int j=0;j<9;j++){
-                if (isEnd) return;
-                if(map[i][j]==0){
-                    for(int num=1;num<=9;num++){
-                        if(isPossible(i,j,num)){
-                            map[i][j]=num;
-                            back(r,c,cnt+1);
-                            map[i][j]=0;
-                        }
-                        if (isEnd) return;
-                    }
-                    if(map[i][j]==0)return;
-                }
+        Point p=list.get(cnt);
+        if (isEnd) return;
+        for(int num=1;num<=9;num++){
+            if(isPossible(p.r,p.c,num)){
+                map[p.r][p.c]=num;
+                back(cnt+1);
+                map[p.r][p.c]=0;
             }
+            if (isEnd) return;
         }
     }
-
-    private static Set<Integer> getNums(int r, int c) {
-        Set<Integer> set=new HashSet<>();
-        for(int i=1;i<=9;i++){
-            set.add(i);
-        }
-
-        for(int i=0;i<9;i++){
-            set.remove(map[r][i]);
-            set.remove(map[i][c]);
-        }
-
-        int nr=r/3*3;
-        int nc=c/3*3;
-        for(int i=nr;i<nr+3;i++){
-            for(int j=nc;j<nc+3;j++){
-                set.remove(map[nr][nc]);
-            }
-        }
-        return set;
-    }
-
     private static boolean isPossible(int r, int c, int num) {
         for(int i=0;i<9;i++){
             if(map[r][i]==num||map[i][c]==num){
