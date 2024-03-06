2916f1d15e6cd35eca69c963effbdad569286e9a
elle6044 <123047819+elle6044@users.noreply.github.com>
2024-03-07 08:56:04 +0900

    [Gold V] Title: 감소하는 수, Time: 76 ms, Memory: 11604 KB -BaekjoonHub

---
+++ 백준/Gold/1038. 감소하는 수/감소하는 수.java
@@ -0,0 +1,36 @@
+import java.io.*;
+
+public class Main {
+    static BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+    static BufferedWriter bw=new BufferedWriter(new OutputStreamWriter(System.out));
+
+    static int N;
+    public static void main(String[] args) throws IOException {
+        N=Integer.parseInt(br.readLine())+1;
+
+        for(int i=1;true;i++){
+            back(i,10,0);
+            if(N==0||i>=10) break;
+        }
+
+        bw.write(answer+"");
+        bw.close();
+    }
+    private static void back(int cnt, int num, long sum) {
+        if(N==0) return;
+        if(cnt==0){
+            N--;
+            if(N==0){
+                answer=sum;
+            }
+            return;
+        }
+
+        for(int i=0;i<num;i++){
+            if(i>cnt-2){
+                back(cnt-1,i,sum*10+i);
+            }
+        }
+    }
+    static long answer=-1;
+}
