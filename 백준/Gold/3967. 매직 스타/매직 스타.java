5c5541adfa3b60a90f9075b3ae3ea43767fbedb6
elle6044 <123047819+elle6044@users.noreply.github.com>
2024-03-07 23:20:51 +0900

    [Gold V] Title: 매직 스타, Time: 604 ms, Memory: 176508 KB -BaekjoonHub

--- 백준/Gold/3967. 매직 스타/매직 스타.java
+++ 백준/Gold/3967. 매직 스타/매직 스타.java
@@ -6,7 +6,7 @@ public class Main {
     static BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
     static BufferedWriter bw=new BufferedWriter(new OutputStreamWriter(System.out));
 
-    static int[][] map=new int[5][9];
+    static char[][] map=new char[5][9];
     static boolean[] v=new boolean[13];
 
     static ArrayList<Point> xList=new ArrayList<>();
@@ -23,15 +23,14 @@ public class Main {
         for(int i=0;i<5;i++){
             String input=br.readLine();
             for(int j=0;j<9;j++){
-                int num=input.charAt(j)-64;
-                if(num>0){
-                    if(num==56){
-                        xList.add(new Point(i,j));
-                    }
-                    else{
-                        map[i][j]=num;
-                        v[num]=true;
-                    }
+                char word=input.charAt(j);
+                map[i][j]=word;
+                if(word=='.') continue;
+                else if (word=='x') {
+                    xList.add(new Point(i,j));
+                }
+                else{
+                    v[word-64]=true;
                 }
             }
         }
@@ -40,12 +39,7 @@ public class Main {
 
         for(int i=0;i<5;i++){
             for(int j=0;j<9;j++){
-                if(map[i][j]==0){
-                    bw.write(".");
-                }
-                else{
-                    bw.write((char)(map[i][j]+64));
-                }
+                bw.write(map[i][j]);
             }
             bw.write("\n");
         }
@@ -66,11 +60,11 @@ public class Main {
         Point p=xList.get(cnt);
         for(int i=1;i<=12;i++){
             if(!v[i]){
-                map[p.r][p.c]=i;
+                map[p.r][p.c]=(char)(i+64);
                 v[i]=true;
                 back(cnt+1);
                 if(check) return;
-                map[p.r][p.c]=0;
+//                map[p.r][p.c]=0;
                 v[i]=false;
             }
         }
@@ -79,12 +73,12 @@ public class Main {
     private static boolean check() {
         int[] sum=new int[6];
         for(int i=0;i<4;i++){
-            sum[0]+=map[1][i*2+1];
-            sum[1]+=map[3][i*2+1];
-            sum[2]+=map[i][4-i];
-            sum[3]+=map[i][4+i];
-            sum[4]+=map[4-i][4-i];
-            sum[5]+=map[4-i][4+i];
+            sum[0]+=map[1][i*2+1]-64;
+            sum[1]+=map[3][i*2+1]-64;
+            sum[2]+=map[i][4-i]-64;
+            sum[3]+=map[i][4+i]-64;
+            sum[4]+=map[4-i][4-i]-64;
+            sum[5]+=map[4-i][4+i]-64;
         }
         for(int i=0;i<6;i++){
             if(sum[i]!=26){
