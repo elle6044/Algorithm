4280048cb66b331adc350375b93e233a5cc7e6d2
elle6044 <123047819+elle6044@users.noreply.github.com>
2024-03-19 16:46:03 +0900

    [D3] Title: 회문의 회문, Time: 101 ms, Memory: 23,856 KB -BaekjoonHub

---
+++ SWEA/D3/20019. 회문의 회문/회문의 회문.java
@@ -0,0 +1,27 @@
+import java.io.*;
+import java.util.*;
+public class Solution {
+	static BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+	static BufferedWriter bw=new BufferedWriter(new OutputStreamWriter(System.out));
+
+	public static void main(String[] args) throws Exception{
+
+		int T=Integer.parseInt(br.readLine());
+		L:for(int t=1;t<=T;t++) {
+			String input=br.readLine();
+			int len=input.length();
+			for(int i=0;i<len/2;i++) {
+				if(input.charAt(i)!=input.charAt(len-1-i)||
+						input.charAt(i)!=input.charAt(len/2-1-i)||
+						input.charAt(len-1-i)!=input.charAt(len/2+1+i)) {
+					bw.write("#"+t+" NO\n");
+					continue L;
+				}
+			}
+			
+			bw.write("#"+t+" YES\n");
+		}
+		bw.close();
+	}
+
+}
