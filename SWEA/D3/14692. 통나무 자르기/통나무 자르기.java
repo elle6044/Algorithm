33739afe335f439bcbb9bb994bd5d39add351356
elle6044 <123047819+elle6044@users.noreply.github.com>
2024-03-21 22:35:28 +0900

    [D3] Title: 통나무 자르기, Time: 106 ms, Memory: 22,132 KB -BaekjoonHub

---
+++ SWEA/D3/14692. 통나무 자르기/통나무 자르기.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.util.*;
+public class Solution {
+	static BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+	static BufferedWriter bw=new BufferedWriter(new OutputStreamWriter(System.out));
+	
+	public static void main(String[] args) throws Exception{
+		int T=Integer.parseInt(br.readLine());
+		for(int t=1;t<=T;t++) {
+			int N=Integer.parseInt(br.readLine());
+			bw.write("#"+t+" "+(N%2==0?"Alice":"Bob")+"\n");
+		}
+		bw.close();
+	}
+
+}
