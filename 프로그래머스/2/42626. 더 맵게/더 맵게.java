9b0bf65bfd88319b9a93952d89cef119e2a3d86c
elle6044 <123047819+elle6044@users.noreply.github.com>
2024-03-29 14:37:55 +0900

    [level 2] Title: 더 맵게, Time: 1703.85 ms, Memory: 124 MB -BaekjoonHub

---
+++ 프로그래머스/2/42626. 더 맵게/더 맵게.java
@@ -0,0 +1,20 @@
+import java.util.*;
+class Solution {
+    public int solution(int[] scoville, int K) {
+        int answer = 0;
+        PriorityQueue<Integer>pq=new PriorityQueue();
+        for(int input:scoville){
+            pq.offer(input);
+        }
+        while(pq.size()>=2&&pq.peek()<K){
+            int num1=pq.poll();
+            int num2=pq.poll();
+            pq.offer(num1+num2*2);
+            answer++;
+        }
+        if(pq.peek()<K){
+            answer=-1;
+        }
+        return answer;
+    }
+}
