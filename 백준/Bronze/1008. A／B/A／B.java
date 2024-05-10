f3cf31e408cc88762b5f7ffb6e4c13439b965ca0
elle6044 <123047819+elle6044@users.noreply.github.com>
2024-05-11 08:54:11 +0900

    [Bronze V] Title: A/B, Time: 108 ms, Memory: 12916 KB -BaekjoonHub

---
+++ 백준/Bronze/1008. A／B/A／B.java
@@ -0,0 +1,11 @@
+import java.util.*;
+import java.io.*;
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		StringTokenizer st=new StringTokenizer(sc.nextLine());
+		System.out.print(Double.parseDouble(st.nextToken())/Double.parseDouble(st.nextToken()));
+	}
+
+}
