e3a344b8f0b77101ba15e6431a1142d2ece0f6aa
elle6044 <123047819+elle6044@users.noreply.github.com>
2024-05-15 16:11:25 +0900

    [Silver V] Title: 수 정렬하기 4, Time: 620 ms, Memory: 117484 KB -BaekjoonHub

---
+++ 백준/Silver/11931. 수 정렬하기 4/수 정렬하기 4.java
@@ -0,0 +1,24 @@
+import java.util.*;
+import java.io.*;
+public class Main {
+
+	static BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+	static BufferedWriter bw=new BufferedWriter(new OutputStreamWriter(System.out));
+	public static void main(String[] args) throws Exception{
+		int N=Integer.parseInt(br.readLine());
+		boolean[] v=new boolean[2000001];
+		for(int i=0;i<N;i++) {
+			v[Integer.parseInt(br.readLine())+1000000]=true;
+		}
+		
+		for(int i=2000000;i>=0;i--) {
+			if(v[i]) {
+				bw.write((i-1000000)+"\n");
+			}
+		}
+		
+		bw.close();
+
+	}
+
+}
