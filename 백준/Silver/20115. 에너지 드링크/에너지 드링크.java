23aab412f285cd6f7beaa8d6ef733acb22dae220
elle6044 <123047819+elle6044@users.noreply.github.com>
2024-05-21 11:08:03 +0900

    [Silver III] Title: 에너지 드링크, Time: 336 ms, Memory: 35512 KB -BaekjoonHub

--- 백준/Silver/20115. 에너지 드링크/에너지 드링크.java
+++ 백준/Silver/20115. 에너지 드링크/에너지 드링크.java
@@ -7,15 +7,15 @@ public class Main {
 	public static void main(String[] args) throws Exception{
 		int N=Integer.parseInt(br.readLine());
 		st=new StringTokenizer(br.readLine());
-		PriorityQueue<Double>pq=new PriorityQueue(Comparator.reverseOrder());
+		double[] drinks=new double[N];
 		for(int i=0;i<N;i++) {
-			pq.offer(Double.parseDouble(st.nextToken()));
+			drinks[i]=Double.parseDouble(st.nextToken());
 		}
+		Arrays.sort(drinks);
 		
-		double answer=pq.poll();
-		while(!pq.isEmpty()) {
-			double drink=pq.poll();
-			answer+=drink/2;
+		double answer=drinks[N-1];
+		for(int i=N-2;i>=0;i--) {
+			answer+=drinks[i]/2;
 		}
 		
 		bw.write(answer+"");
