096e34bef884bcf8152c2e7ed73098d229a42856
elle6044 <123047819+elle6044@users.noreply.github.com>
2024-05-22 12:28:59 +0900

    [Silver III] Title: 먹을 것인가 먹힐 것인가, Time: 480 ms, Memory: 47804 KB -BaekjoonHub

--- 백준/Silver/7795. 먹을 것인가 먹힐 것인가/먹을 것인가 먹힐 것인가.java
+++ 백준/Silver/7795. 먹을 것인가 먹힐 것인가/먹을 것인가 먹힐 것인가.java
@@ -4,40 +4,52 @@ public class Main {
 	static BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
 	static BufferedWriter bw=new BufferedWriter(new OutputStreamWriter(System.out));
 	static StringTokenizer st;
+	
+	static int N,M;
+	static int[] A;
+	static int[] B;
 	public static void main(String[] args) throws Exception{
 		int T=Integer.parseInt(br.readLine());
 		for(int t=0;t<T;t++) {
 			st=new StringTokenizer(br.readLine());
-			int N=Integer.parseInt(st.nextToken());
-			int M=Integer.parseInt(st.nextToken());
-			PriorityQueue<Integer> A=new PriorityQueue(Comparator.reverseOrder());
-			PriorityQueue<Integer> B=new PriorityQueue(Comparator.reverseOrder());
+			N=Integer.parseInt(st.nextToken());
+			M=Integer.parseInt(st.nextToken());
+			
+			A=new int[N];
+			B=new int[M];
 			
 			st=new StringTokenizer(br.readLine());
 			for(int i=0;i<N;i++) {
-				A.offer(Integer.parseInt(st.nextToken()));
+				A[i]=Integer.parseInt(st.nextToken());
 			}
 			
 			st=new StringTokenizer(br.readLine());
 			for(int i=0;i<M;i++) {
-				B.offer(Integer.parseInt(st.nextToken()));
+				B[i]=Integer.parseInt(st.nextToken());
 			}
+			Arrays.sort(B);
 			
 			int answer=0;
-			while(!A.isEmpty()&&!B.isEmpty()) {
-				int a=A.peek();
-				int b=B.peek();
-				if(a>b) {
-					answer+=B.size();
-					A.poll();
-				}
-				else {
-					B.poll();
+			for(int i=0;i<N;i++) {
+				int size=A[i];
+				
+				int high=M-1;
+				int low=0;
+				
+				while(high>=low) {
+					int mid=(high+low)/2;
+					
+					if(B[mid]<size) {
+						answer+=mid-low+1;
+						low=mid+1;
+					}
+					else {
+						high=mid-1;
+					}
 				}
 			}
 			
 			bw.write(answer+"\n");
-			
 		}
 		bw.close();
 	}
