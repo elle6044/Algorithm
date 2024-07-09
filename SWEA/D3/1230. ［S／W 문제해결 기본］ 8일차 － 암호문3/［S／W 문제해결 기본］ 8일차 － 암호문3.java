59e809c71dfde0ccdf81e383ebe3cfb5e136ee28
elle6044 <123047819+elle6044@users.noreply.github.com>
2024-07-09 17:00:12 +0900

    [D3] Title: [S/W 문제해결 기본] 8일차 - 암호문3, Time: 160 ms, Memory: 27,840 KB -BaekjoonHub

--- SWEA/D3/1230. ［S／W 문제해결 기본］ 8일차 － 암호문3/［S／W 문제해결 기본］ 8일차 － 암호문3.java
+++ SWEA/D3/1230. ［S／W 문제해결 기본］ 8일차 － 암호문3/［S／W 문제해결 기본］ 8일차 － 암호문3.java
@@ -8,60 +8,44 @@ public class Solution {
 	public static void main(String[] args) throws Exception{
 		for(int t=1;t<=10;t++) {
 			int N=Integer.parseInt(br.readLine());
-			Deque<Integer> dq=new ArrayDeque();
+			List<Integer> list=new ArrayList();
 			st=new StringTokenizer(br.readLine());
 			for(int i=0;i<N;i++) {
-				dq.offerLast(Integer.parseInt(st.nextToken()));
+				list.add(Integer.parseInt(st.nextToken()));
 			}
 			
 			int M=Integer.parseInt(br.readLine());
-			Stack<Integer> stack=new Stack();
 			
 			st=new StringTokenizer(br.readLine());
 			for(int i=0;i<M;i++) {
-				
 				String com=st.nextToken();
 				if(com.equals("I")) {
 					int x=Integer.parseInt(st.nextToken());
 					int y=Integer.parseInt(st.nextToken());
 					
-					for(int j=0;j<x;j++) {
-						stack.push(dq.pollFirst());
-					}
 					for(int j=0;j<y;j++) {
-						stack.push(Integer.parseInt(st.nextToken()));
-					}
-					
-					while(!stack.isEmpty()) {
-						dq.offerFirst(stack.pop());
+						list.add(x+j,Integer.parseInt(st.nextToken()));
 					}
 				}
 				else if(com.equals("D")) {
 					int x=Integer.parseInt(st.nextToken());
 					int y=Integer.parseInt(st.nextToken());
 					
-					for(int j=0;j<x;j++) {
-						stack.push(dq.pollFirst());
-					}
 					for(int j=0;j<y;j++) {
-						dq.pollFirst();
-					}
-					
-					while(!stack.isEmpty()) {
-						dq.offerFirst(stack.pop());
+						list.remove(x);
 					}
 				}
 				else if(com.equals("A")) {
 					int y=Integer.parseInt(st.nextToken());
 					for(int j=0;j<y;j++) {
-						dq.offerLast(Integer.parseInt(st.nextToken()));
+						list.add(Integer.parseInt(st.nextToken()));
 					}
 				}
 			}
 			
 			bw.write("#"+t);
 			for(int i=0;i<10;i++) {
-				bw.write(" "+dq.pollFirst());
+				bw.write(" "+list.get(i));
 			}
 			bw.write("\n");
 		}
