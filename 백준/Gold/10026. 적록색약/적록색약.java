50a2782dbc2e0206b6455c94a61edf5814456a8e
elle6044 <123047819+elle6044@users.noreply.github.com>
2025-01-11 19:15:28 +0900

    [Gold V] Title: 적록색약, Time: 76 ms, Memory: 12648 KB -BaekjoonHub

--- 백준/Gold/10026. 적록색약/적록색약.java
+++ 백준/Gold/10026. 적록색약/적록색약.java
@@ -1,108 +1,77 @@
+import java.util.*;
 import java.io.*;
-import java.util.LinkedList;
-import java.util.Queue;
 
 public class Main {
-    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-
-    static int N;
-    static char[][] map;
-    static boolean[][] visit;
-
-    public static void main(String[] args) throws IOException {
-        N=Integer.parseInt(br.readLine());
-        map=new char[N][N];
-        visit=new boolean[N][N];
-        for(int i=0;i<N;i++){
-            String word = br.readLine();
-            for(int j=0;j<N;j++){
-                map[i][j]=word.charAt(j);
-            }
-        }
-
-        int count1=0;
-        for(int i=0;i<N;i++){
-            for(int j=0;j<N;j++){
-                if(!visit[i][j]){
-                    bfs1(i,j);
-                    count1++;
-                }
-            }
-        }
-
-        int count2=0;
-        for(int i=0;i<N;i++){
-            for(int j=0;j<N;j++){
-                if(visit[i][j]){
-                    bfs2(i,j);
-                    count2++;
-                }
-            }
-        }
-
-        bw.write(count1+" "+count2);
-        bw.close();
-
-    }
-
-    static int[] dr={1,-1,0,0};
-    static int[] dc={0,0,1,-1};
-
-    static class Point{
-        int r,c;
-        char type;
-
-        public Point(int r, int c, char type){
-            this.r=r;
-            this.c=c;
-            this.type=type;
-        }
-    }
-
-    static void bfs1(int r, int c){
-        Queue<Point> queue = new LinkedList<>();
-        visit[r][c]=true;
-        queue.offer(new Point(r,c,map[r][c]));
-
-        while(!queue.isEmpty()){
-            Point p = queue.poll();
-            for(int d=0;d<4;d++){
-                int nr=p.r+dr[d];
-                int nc=p.c+dc[d];
-                if(nr>=0&&nr<N&&nc>=0&&nc<N&&p.type==map[nr][nc]&&!visit[nr][nc]){
-                    visit[nr][nc]=true;
-                    queue.offer(new Point(nr,nc,p.type));
-                }
-            }
-        }
-    }
-
-    static void bfs2(int r, int c){
-        Queue<Point> queue = new LinkedList<>();
-        visit[r][c]=false;
-        queue.offer(new Point(r,c,map[r][c]));
-
-        while(!queue.isEmpty()){
-            Point p = queue.poll();
-            for(int d=0;d<4;d++){
-                int nr=p.r+dr[d];
-                int nc=p.c+dc[d];
-                if(nr>=0&&nr<N&&nc>=0&&nc<N&&visit[nr][nc]){
-                    if(p.type=='B'){
-                        if(map[nr][nc]=='B'){
-                            visit[nr][nc]=false;
-                            queue.offer(new Point(nr,nc,p.type));
-                        }
-                    }
-                    else{
-                        if(map[nr][nc]=='R'||map[nr][nc]=='G'){
-                            visit[nr][nc]=false;
-                            queue.offer(new Point(nr,nc,map[nr][nc]));
-                        }
-                    }
-                }
-            }
-        }
-    }
+	static BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+	static BufferedWriter bw=new BufferedWriter(new OutputStreamWriter(System.out));
+	
+	static int N;
+	static int[][] map;
+	static boolean[][] v;
+	
+	static int[] dr= {1,-1,0,0};
+	static int[] dc= {0,0,1,-1};
+	
+	static class Point{
+		int r,c;
+		public Point(int r, int c) {
+			this.r=r;
+			this.c=c;
+		}
+	}
+	
+	public static void main(String[] args) throws Exception{
+		N=Integer.parseInt(br.readLine());
+		map=new int[N][N];
+		for(int i=0;i<N;i++) {
+			String input=br.readLine();
+			for(int j=0;j<N;j++) {
+				char word=input.charAt(j);
+				int num=2;
+				if(word=='R') {
+					num=1;
+				}
+				else if(word=='G') {
+					num=3;
+				}
+				map[i][j]=num;
+			}
+		}
+		
+		int[] answer=new int[2];
+		
+		for(int t=0;t<2;t++) {
+			v=new boolean[N][N];
+			for(int i=0;i<N;i++) {
+				for(int j=0;j<N;j++) {
+					if(!v[i][j]) {
+						bfs(i,j,map[i][j]);
+						answer[t]++;
+					}
+				}
+			}
+		}
+		
+		bw.write(answer[0]+" "+answer[1]);
+		bw.close();
+	}
+	
+	static void bfs(int r, int c, int color) {
+		Queue<Point>q=new ArrayDeque();
+		q.offer(new Point(r,c));
+		v[r][c]=true;
+		
+		while(!q.isEmpty()) {
+			Point p=q.poll();
+			map[p.r][p.c]%=2;
+			for(int d=0;d<4;d++) {
+				int nr=p.r+dr[d];
+				int nc=p.c+dc[d];
+				if(nr>=0&&nr<N&&nc>=0&&nc<N&&map[nr][nc]==color&&!v[nr][nc]) {
+					q.offer(new Point(nr,nc));
+					v[nr][nc]=true;
+				}
+			}
+		}
+	}
 }
