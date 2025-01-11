44ed54ea7808f663ab997bf4858c45edf3ca6f7b
elle6044 <123047819+elle6044@users.noreply.github.com>
2025-01-11 19:58:35 +0900

    [Gold III] Title: 벽 부수고 이동하기, Time: 452 ms, Memory: 88352 KB -BaekjoonHub

--- 백준/Gold/2206. 벽 부수고 이동하기/벽 부수고 이동하기.java
+++ 백준/Gold/2206. 벽 부수고 이동하기/벽 부수고 이동하기.java
@@ -1,81 +1,79 @@
+import java.util.*;
 import java.io.*;
-import java.util.ArrayDeque;
-import java.util.Queue;
-import java.util.StringTokenizer;
 
 public class Main {
-    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-    static StringTokenizer st;
-
-    static int N,M;
-    static int[][] map;
-    static boolean[][][] v;
-
-    public static void main(String[] args) throws IOException {
-        st=new StringTokenizer(br.readLine());
-        N=Integer.parseInt(st.nextToken());
-        M=Integer.parseInt(st.nextToken());
-
-        map=new int[N][M];
-        v=new boolean[2][N][M];
-
-        for(int i=0;i<N;i++){
-            String word=br.readLine();
-            for(int j=0;j<M;j++){
-                map[i][j]=Character.getNumericValue(word.charAt(j));
-            }
-        }
-
-        bfs();
-        if(minCnt==Integer.MAX_VALUE) minCnt=-1;
-        bw.write(minCnt+"");
-        bw.close();
-    }
-
-    static class Point{
-        int h,r,c,cnt;
-        public Point(int h, int r, int c, int cnt){
-            this.h=h;
-            this.r=r;
-            this.c=c;
-            this.cnt=cnt;
-        }
-    }
-
-    static int[] dr={1,-1,0,0};
-    static int[] dc={0,0,1,-1};
-
-    static int minCnt = Integer.MAX_VALUE;
-    private static void bfs() {
-        Queue<Point> q = new ArrayDeque<>();
-        v[0][0][0]=true;
-        q.offer(new Point(0,0,0,1));
-        while(!q.isEmpty()){
-            Point p=q.poll();
-            if(p.r==N-1&&p.c==M-1&&p.cnt<minCnt){
-                minCnt=p.cnt;
-            }
-            for(int d=0;d<4;d++){
-                int nr = p.r+dr[d];
-                int nc = p.c+dc[d];
-                if(nr>=0&&nr<N&&nc>=0&&nc<M){
-                    if(map[nr][nc]==0){
-                        if(!v[p.h][nr][nc]){
-                            v[p.h][nr][nc]=true;
-                            q.offer(new Point(p.h,nr,nc,p.cnt+1));
-                        }
-                    }
-                    else{
-                        if(p.h==0){
-                            if(!v[p.h+1][nr][nc]){
-                                v[p.h+1][nr][nc]=true;
-                                q.offer(new Point(p.h+1,nr,nc,p.cnt+1));
-                            }
-                        }
-                    }
-                }
-            }
-        }
-    }
+	static BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+	static BufferedWriter bw=new BufferedWriter(new OutputStreamWriter(System.out));
+	static StringTokenizer st;
+	
+	static int N,M;
+	static int[][] map;
+	static boolean[][][] v;
+	
+	static int[] dr= {1,-1,0,0};
+	static int[] dc= {0,0,1,-1};
+	
+	static class Point{
+		int r,c,wall,cost;
+		public Point(int r, int c, int wall, int cost) {
+			this.r=r;
+			this.c=c;
+			this.wall=wall;
+			this.cost=cost;
+		}
+	}
+	
+	public static void main(String[] args) throws Exception{
+		st=new StringTokenizer(br.readLine());
+		N=Integer.parseInt(st.nextToken());
+		M=Integer.parseInt(st.nextToken());
+		
+		map=new int[N][M];
+		v=new boolean[2][N][M];
+		
+		for(int i=0;i<N;i++) {
+			String input=br.readLine();
+			for(int j=0;j<M;j++) {
+				char word=input.charAt(j);
+				map[i][j]=Character.getNumericValue(word);	
+			}
+		}
+		
+		bfs();
+		
+		bw.write((answer==Integer.MAX_VALUE?-1:answer)+"");
+		bw.close();
+	}
+	
+	static int answer=Integer.MAX_VALUE;
+	
+	static void bfs() {
+		Queue<Point> q=new ArrayDeque();
+		q.offer(new Point(0,0,0,1));
+		v[0][0][0]=true;
+		
+		while(!q.isEmpty()) {
+			Point p=q.poll();
+			if(p.r==N-1&&p.c==M-1) {
+				answer=Math.min(answer, p.cost);
+			}
+			
+			for(int d=0;d<4;d++) {
+				int nr=p.r+dr[d];
+				int nc=p.c+dc[d];
+				if(nr>=0&&nr<N&&nc>=0&&nc<M&&!v[p.wall][nr][nc]) {
+					if(map[nr][nc]==0) {
+						q.offer(new Point(nr,nc,p.wall,p.cost+1));
+						v[p.wall][nr][nc]=true;
+					}
+					else {
+						if(p.wall==0) {
+							q.offer(new Point(nr,nc,p.wall+1,p.cost+1));
+							v[p.wall+1][nr][nc]=true;
+						}
+					}
+				}
+			}
+		}
+	}
 }
