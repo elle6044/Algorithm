e687d8e8c324881080c4f1c9070420131f33ec86
elle6044 <123047819+elle6044@users.noreply.github.com>
2025-01-12 13:00:53 +0900

    [Gold IV] Title: 알파벳, Time: 892 ms, Memory: 12516 KB -BaekjoonHub

--- 백준/Gold/1987. 알파벳/알파벳.java
+++ 백준/Gold/1987. 알파벳/알파벳.java
@@ -1,57 +1,54 @@
+import java.util.*;
 import java.io.*;
-import java.util.HashSet;
-import java.util.Map;
-import java.util.Set;
-import java.util.StringTokenizer;
 
 public class Main {
-    static BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
-    static BufferedWriter bw=new BufferedWriter(new OutputStreamWriter(System.out));
-    static StringTokenizer st;
+	static BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+	static BufferedWriter bw=new BufferedWriter(new OutputStreamWriter(System.out));
+	static StringTokenizer st;
 
-    static int R,C;
-    static int[][] map;
+	static int N,M;
+	static int[][] map;
+	static boolean[] v=new boolean[26];
+	
+	static int[] dr= {1,-1,0,0};
+	static int[] dc= {0,0,1,-1};
+	
+	public static void main(String[] args) throws Exception{
+		st=new StringTokenizer(br.readLine());
+		N=Integer.parseInt(st.nextToken());
+		M=Integer.parseInt(st.nextToken());
+		
+		map=new int[N][M]; 
+		for(int i=0;i<N;i++) {
+			String input=br.readLine();
+			for(int j=0;j<M;j++) {
+				char word=input.charAt(j);
+				map[i][j]=(int)word - 65;
+			}
+		}
+		
+		v[map[0][0]]=true;
+		dfs(0,0,1);
+		
+		bw.write(answer+"");
+		bw.close();
+		
+	}
+	
+	static int answer=Integer.MIN_VALUE;
+	
+	static void dfs(int r, int c, int cnt) {
+		answer=Math.max(answer, cnt);
+		for(int d=0;d<4;d++) {
+			int nr=r+dr[d];
+			int nc=c+dc[d];
+			if(nr>=0&&nr<N&&nc>=0&&nc<M&&!v[map[nr][nc]]) {
+				v[map[nr][nc]]=true;
+				dfs(nr, nc, cnt+1);
+				v[map[nr][nc]]=false;
+			}
+		}
+	}
+	
 
-
-    static int[] dr={1,-1,0,0};
-    static int[] dc={0,0,1,-1};
-
-    static boolean[] words=new boolean[26];
-
-    public static void main(String[] args) throws IOException {
-        st=new StringTokenizer(br.readLine());
-        R=Integer.parseInt(st.nextToken());
-        C=Integer.parseInt(st.nextToken());
-        map=new int[R][C];
-        for(int i=0;i<R;i++){
-            String input=br.readLine();
-            for(int j=0;j<C;j++){
-                map[i][j]=input.charAt(j)-65;
-            }
-        }
-        words[map[0][0]]=true;
-
-        back(0,0, 1);
-
-        bw.write(answer+"");
-        bw.close();
-    }
-
-    static int answer=1;
-    private static void back(int r, int c, int cnt) {
-
-        for(int d=0;d<4;d++){
-            int nr=r+dr[d];
-            int nc=c+dc[d];
-            if(nr>=0&&nr<R&&nc>=0&&nc<C){
-                int next=map[nr][nc];
-                if(!words[next]){
-                    words[next]=true;
-                    answer=Math.max(answer,cnt+1);
-                    back(nr,nc,cnt+1);
-                    words[next]=false;
-                }
-            }
-        }
-    }
 }
