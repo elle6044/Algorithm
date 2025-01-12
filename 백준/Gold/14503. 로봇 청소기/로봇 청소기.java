884e4cd7538aec0dc5b05c498ab3ac5aa92f7589
elle6044 <123047819+elle6044@users.noreply.github.com>
2025-01-12 15:51:26 +0900

    [Gold V] Title: 로봇 청소기, Time: 72 ms, Memory: 11944 KB -BaekjoonHub

--- 백준/Gold/14503. 로봇 청소기/로봇 청소기.java
+++ 백준/Gold/14503. 로봇 청소기/로봇 청소기.java
@@ -1,89 +1,115 @@
-import java.io.BufferedWriter;
-import java.io.OutputStreamWriter;
-import java.util.ArrayDeque;
-import java.util.Queue;
+import java.util.*;
+import java.io.*;
 
 public class Main {
-    static BufferedWriter bw=new BufferedWriter(new OutputStreamWriter(System.out));
-    static int N,M;
-    static int[][]map;
-    static int[] dr={-1,0,1,0};
-    static int[] dc={0,1,0,-1};
-    static class Point{
-        int r,c,d;
-        public Point(int r, int c, int d){
-            this.r=r;
-            this.c=c;
-            this.d=d;
-        }
-    }
+	static BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+	static BufferedWriter bw=new BufferedWriter(new OutputStreamWriter(System.out));
+	static StringTokenizer st;
+	
+	static int N,M;
+	static int[][] map;
+	
+	static int[] dr= {-1,0,1,0};
+	static int[] dc= {0,1,0,-1};
+		
+	static class Robot{
+		int r,c,d,score;
+		public Robot(int r, int c, int d, int score) {
+			this.r=r;
+			this.c=c;
+			this.d=d;
+			this.score=score;
+		}
+	}
+	
+	static Robot robot;
+	
+	public static void main(String[] args) throws Exception{
+		st=new StringTokenizer(br.readLine());
+		N=Integer.parseInt(st.nextToken());
+		M=Integer.parseInt(st.nextToken());
+		
+		st=new StringTokenizer(br.readLine());
+		int sr=Integer.parseInt(st.nextToken());
+		int sc=Integer.parseInt(st.nextToken());
+		int sd=Integer.parseInt(st.nextToken());
+		robot=new Robot(sr, sc, sd, 0);
+		
+		map=new int[N][M];
+		for(int i=0;i<N;i++) {
+			st=new StringTokenizer(br.readLine());
+			for(int j=0;j<M;j++) {
+				map[i][j]=Integer.parseInt(st.nextToken());
+			}
+		}
+		
+		while(true) {
+			clear();
 
-    public static void main(String[] args) throws Exception {
-        N=nextInt();
-        M=nextInt();
-        map=new int[N][M];
-        int sr=nextInt();
-        int sc=nextInt();
-        int sd=nextInt();
-        for(int i=0;i<N;i++){
-            for(int j=0;j<M;j++){
-                map[i][j]=nextInt();
-            }
-        }
-
-        bfs(sr,sc,sd);
-
-        bw.write(cnt+"");
-        bw.close();
-    }
-
-    static int cnt=1;
-    private static void bfs(int sr, int sc, int sd) {
-        Queue<Point>q=new ArrayDeque<>();
-        q.offer(new Point(sr,sc,sd));
-        map[sr][sc]=-1;
-
-        L:while(!q.isEmpty()){
-            Point p=q.poll();
-
-            for(int d=0;d<4;d++){
-                int nd=p.d;
-                for(int d1=0;d1<=d;d1++){
-                    nd=nd==0?3:nd-1;
-                }
-                int nr=p.r+dr[nd];
-                int nc=p.c+dc[nd];
-                if(map[nr][nc]==0){
-                    q.offer(new Point(nr,nc,nd));
-                    map[nr][nc]=-1;
-                    cnt++;
-                    continue L;
-                }
-            }
-            int bd=p.d<2?p.d+2:p.d-2;
-            int br=p.r+dr[bd];
-            int bc=p.c+dc[bd];
-            if(map[br][bc]==-1){
-                q.offer(new Point(br,bc,p.d));
-            }
-            else{
-                return;
-            }
-        }
-    }
-
-    static int nextInt() throws Exception {
-        int sign = 0;
-        int n = 0;
-        int c;
-        while ((c = System.in.read()) <= 32);
-        if(c == 45) {
-            sign = 1;
-            c = System.in.read();
-        }
-        do n = (n << 3) + (n << 1) + (c & 15);
-        while ((c = System.in.read()) > 47 && c < 58);
-        return sign == 0 ? n : ~n + 1;
-    }
-
+			int dir=checkDir();
+			if(dir==-1) {
+				if(checkBack()) {
+					back();
+				}
+				else {
+					break;
+				}
+			}
+			else {
+				changeDir(dir);
+				move();
+			}
+		}
+		
+		bw.write(robot.score+"");
+		bw.close();
+	}
+	
+	static void move() {
+		robot.r+=dr[robot.d];
+		robot.c+=dc[robot.d];
+	}
+	
+	static void changeDir(int dir) {
+		robot.d=dir;
+	}
+	
+	static void back() {
+		robot.r+=dr[(robot.d+2)%4];
+		robot.c+=dc[(robot.d+2)%4];
+	}
+	
+	static boolean checkBack() {
+		int nr=robot.r+dr[(robot.d+2)%4];
+		int nc=robot.c+dc[(robot.d+2)%4];
+		if(nr>=0&&nr<N&&nc>=0&&nc<M&&map[nr][nc]!=1) {
+			return true;
+		}
+		else {
+			return false;
+		}
+	}
+	
+	
+	static int checkDir() {
+		int dir=-1;
+		for(int d=3;d>=0;d--) {
+			int nd=(robot.d+d)%4;
+			int nr=robot.r+dr[nd];
+			int nc=robot.c+dc[nd];
+			if(nr>=0&&nr<N&&nc>=0&&nc<M&&map[nr][nc]==0) {
+				dir=nd;
+				break;
+			}
+		}
+		return dir;
+	}
+	
+	static void clear() {
+		if(map[robot.r][robot.c]==0) {
+			robot.score++;
+			map[robot.r][robot.c]=-1;
+		}
+	}
+	
 }
