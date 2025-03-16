a41e60f40845103b4e6721276acdb52772bf181f
elle6044 <123047819+elle6044@users.noreply.github.com>
2025-03-16 19:03:48 +0900

    [Gold IV] Title: 도시 분할 계획, Time: 1428 ms, Memory: 325316 KB -BaekjoonHub

---
+++ 백준/Gold/1647. 도시 분할 계획/도시 분할 계획.java
@@ -0,0 +1,95 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+	static BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+	static BufferedWriter bw=new BufferedWriter(new OutputStreamWriter(System.out));
+	static StringTokenizer st;
+	
+	static int V,E;
+	static int[] parent,rank;
+	static List<Edge> edgeList;
+	
+	public static void main(String[] args) throws Exception{
+		st=new StringTokenizer(br.readLine());
+		V=Integer.parseInt(st.nextToken());
+		E=Integer.parseInt(st.nextToken());
+		
+		edgeList=new ArrayList<>(E);
+		for(int i=0;i<E;i++) {
+			st=new StringTokenizer(br.readLine());
+			int s=Integer.parseInt(st.nextToken())-1;
+			int e=Integer.parseInt(st.nextToken())-1;
+			int w=Integer.parseInt(st.nextToken());
+			
+			edgeList.add(new Edge(s,e,w));
+		}
+		Collections.sort(edgeList);
+		
+		parent=new int[V];
+		rank=new int[V];
+		for(int i=0;i<V;i++) {
+			parent[i]=i;
+			rank[i]=0;
+		}
+		
+		int edgeCnt=0;
+		int answer=0;
+		int maxW=0;
+		
+		for(Edge edge : edgeList) {
+			int rootA=find(edge.s);
+			int rootB=find(edge.e);
+			if(rootA!=rootB) {
+				edgeCnt++;
+				answer+=edge.w;
+				maxW=Math.max(maxW, edge.w);
+				union(rootA,rootB);
+			}
+			
+			if(edgeCnt==V-1) break;
+		}
+		
+		bw.write((answer-maxW)+"");
+		bw.close();
+	}
+	
+	static int find(int node) {
+		if(parent[node]!=node) {
+			parent[node]=find(parent[node]);
+		}
+		return parent[node];
+	}
+	
+	static void union(int a, int b) {
+		int rootA=find(a);
+		int rootB=find(b);
+		
+		if(rootA!=rootB) {
+			if(rank[rootA]>rank[rootB]) {
+				parent[rootB]=rootA;
+			}
+			else if(rank[rootA]<rank[rootB]) {
+				parent[rootA]=rootB;
+			}
+			else {
+				parent[rootA]=rootB;
+				rank[rootA]++;
+			}
+		}
+	}
+	
+	static class Edge implements Comparable<Edge>{
+		int s,e,w;
+		public Edge(int s, int e, int w) {
+			this.s=s;
+			this.e=e;
+			this.w=w;
+		}
+		
+		@Override
+		public int compareTo(Edge o) {
+			return Integer.compare(this.w, o.w);
+		}
+	}
+}
