635e0aa6d0a06958eb8e9b87124ebceba609b046
elle6044 <123047819+elle6044@users.noreply.github.com>
2025-04-17 07:55:40 +0900

    [Silver III] Title: 바이러스, Time: 68 ms, Memory: 11724 KB -BaekjoonHub

--- 백준/Silver/2606. 바이러스/바이러스.java
+++ 백준/Silver/2606. 바이러스/바이러스.java
@@ -1,5 +1,6 @@
 import java.util.*;
 import java.io.*;
+
 public class Main {
 	static BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
 	static BufferedWriter bw=new BufferedWriter(new OutputStreamWriter(System.out));
@@ -7,41 +8,44 @@ public class Main {
 	
 	static int N,M;
 	static List<Integer>[] lists;
+	static boolean[] v;
 	
 	public static void main(String[] args) throws Exception {
 		N=Integer.parseInt(br.readLine());
 		M=Integer.parseInt(br.readLine());
-		lists=new ArrayList[N];
-		for(int i=0;i<N;i++) {
-			lists[i]=new ArrayList();
+		
+		lists=new ArrayList[N+1];
+		for(int i=0;i<N+1;i++) {
+			lists[i]=new ArrayList<Integer>();
 		}
 		
 		for(int i=0;i<M;i++) {
 			st=new StringTokenizer(br.readLine());
-			int s=Integer.parseInt(st.nextToken())-1;
-			int e=Integer.parseInt(st.nextToken())-1;
+			int s=Integer.parseInt(st.nextToken());
+			int e=Integer.parseInt(st.nextToken());
+			
 			lists[s].add(e);
 			lists[e].add(s);
 		}
 		
-		Queue<Integer> q=new ArrayDeque();
-		boolean[] v=new boolean[N];
-		q.offer(0);
-		v[0]=true;
-		int cnt=0;
-		while(!q.isEmpty()) {
-			int a=q.poll();
-			for(int next:lists[a]) {
-				if(!v[next]) {
-					q.add(next);
-					v[next]=true;
-					cnt++;
-				}
+		v=new boolean[N+1];
+		v[1]=true;
+		dfs(1);
+		
+		bw.write(answer+"");
+		bw.close();
+	}
+	
+	static int answer=0;
+	
+	static void dfs(int node) {
+		for(int next : lists[node]) {
+			if(!v[next]) {
+				answer++;
+				v[next]=true;
+				dfs(next);
 			}
 		}
-		
-		bw.write(cnt+"");
-		bw.close();
 	}
 
 }
