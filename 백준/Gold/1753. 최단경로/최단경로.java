c724b34c26f45550c85b4bb90034904fd2ca454d
elle6044 <123047819+elle6044@users.noreply.github.com>
2025-04-17 11:05:43 +0900

    [Gold IV] Title: 최단경로, Time: 616 ms, Memory: 117744 KB -BaekjoonHub

--- 백준/Gold/1753. 최단경로/최단경로.java
+++ 백준/Gold/1753. 최단경로/최단경로.java
@@ -1,76 +1,72 @@
+import java.util.*;
 import java.io.*;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.PriorityQueue;
-import java.util.StringTokenizer;
 
 public class Main {
-    static BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
-    static BufferedWriter bw=new BufferedWriter(new OutputStreamWriter(System.out));
-    static StringTokenizer st;
-
-    static int V,E,K;
-    static ArrayList<Point>[] array;
-    static int[] dist;
-
-    static class Point implements Comparable<Point>{
-        int e,w;
-        public Point(int e,int w){
-            this.e=e;
-            this.w=w;
-        }
-
-        @Override
-        public int compareTo(Point o) {
-            return this.w-o.w;
-        }
-    }
-
-    public static void main(String[] args) throws IOException {
-        st=new StringTokenizer(br.readLine());
-        V=Integer.parseInt(st.nextToken());
-        E=Integer.parseInt(st.nextToken());
-        K=Integer.parseInt(br.readLine());
-
-        array=new ArrayList[V];
-        for(int i=0;i<V;i++){
-            array[i]=new ArrayList<>();
-        }
-        dist=new int[V];
-        Arrays.fill(dist,Integer.MAX_VALUE);
-
-        for(int i=0;i<E;i++){
-            st=new StringTokenizer(br.readLine());
-            int s=Integer.parseInt(st.nextToken())-1;
-            int e=Integer.parseInt(st.nextToken())-1;
-            int w=Integer.parseInt(st.nextToken());
-            array[s].add(new Point(e,w));
-        }
-
-        PriorityQueue<Point> q=new PriorityQueue<>();
-        dist[K-1]=0;
-        q.add(new Point(K-1,0));
-        while(!q.isEmpty()){
-            Point p=q.poll();
-            if(dist[p.e]<p.w) continue;
-
-            for(Point np : array[p.e]){
-                int nw=np.w+dist[p.e];
-                if(nw<dist[np.e]){
-                    dist[np.e]=nw;
-                    q.add(new Point(np.e,nw));
-                }
-            }
-        }
-
-        for(int i=0;i<V;i++){
-            if(dist[i]==Integer.MAX_VALUE){
-                bw.write("INF\n");
-            }
-            else{
-                bw.write(dist[i]+"\n");
-            }
-        }
-        bw.close();
-    }
+	static BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+	static BufferedWriter bw=new BufferedWriter(new OutputStreamWriter(System.out));
+	static StringTokenizer st;
+	
+	static int V,E,K;
+	static List<Edge> edgeList[];
+	
+	static class Edge implements Comparable<Edge>{
+		int e,w;
+		public Edge(int e, int w) {
+			this.e=e;
+			this.w=w;
+		}
+		@Override
+		public int compareTo(Edge o){
+			return this.w-o.w;
+		}
+	}
+	
+	static int[] dist;
+	
+	public static void main(String[] args) throws Exception{
+		st=new StringTokenizer(br.readLine());
+		V=Integer.parseInt(st.nextToken());
+		E=Integer.parseInt(st.nextToken());
+		K=Integer.parseInt(br.readLine())-1;
+		
+		edgeList=new ArrayList[V];
+		for(int i=0;i<V;i++) {
+			edgeList[i]=new ArrayList<Edge>();
+		}
+		
+		for(int i=0;i<E;i++) {
+			st=new StringTokenizer(br.readLine());
+			int s=Integer.parseInt(st.nextToken())-1;
+			int e=Integer.parseInt(st.nextToken())-1;
+			int w=Integer.parseInt(st.nextToken());
+			
+			edgeList[s].add(new Edge(e,w));
+		}
+		
+		dist=new int[V];
+		Arrays.fill(dist, Integer.MAX_VALUE);
+		dist[K]=0;
+		PriorityQueue<Edge> pq=new PriorityQueue<>();
+		pq.offer(new Edge(K,0));
+		
+		while(!pq.isEmpty()) {
+			Edge edge=pq.poll();
+			
+			if(edge.w>dist[edge.e]) continue;
+			
+			for(Edge next:edgeList[edge.e]) {
+				int nw=dist[edge.e]+next.w;
+				if(nw<dist[next.e]) {
+					dist[next.e]=nw;
+					pq.offer(new Edge(next.e,nw));
+				}
+			}
+		}
+		
+		for(int i=0;i<V;i++) {
+			if(dist[i]==Integer.MAX_VALUE) bw.write("INF\n");
+			else bw.write(dist[i]+"\n");
+		}
+		bw.close();
+	}
 }
