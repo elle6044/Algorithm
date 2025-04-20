f27a932eefe98321cbc5080e76d233537a0d82d8
elle6044 <123047819+elle6044@users.noreply.github.com>
2025-04-20 12:44:09 +0900

    [Gold IV] Title: 연구소, Time: 260 ms, Memory: 76356 KB -BaekjoonHub

--- 백준/Gold/14502. 연구소/연구소.java
+++ 백준/Gold/14502. 연구소/연구소.java
@@ -13,6 +13,8 @@ public class Main {
 	static int[] dr= {1,-1,0,0};
 	static int[] dc= {0,0,1,-1};
 	
+	static List<Point> virusList=new ArrayList<>();
+	
 	static class Point{
 		int r,c;
 		public Point(int r, int c) {
@@ -21,88 +23,82 @@ public class Main {
 		}
 	}
 	
-	static Queue<Point> virus=new ArrayDeque();
+	static int emptySize=0;
 	
 	public static void main(String[] args) throws Exception{
 		st=new StringTokenizer(br.readLine());
+		
 		N=Integer.parseInt(st.nextToken());
 		M=Integer.parseInt(st.nextToken());
-		map=new int[N][M];
-		v=new boolean[N][M];
 		
+		map=new int[N][M];
 		for(int i=0;i<N;i++) {
 			st=new StringTokenizer(br.readLine());
 			for(int j=0;j<M;j++) {
 				int input=Integer.parseInt(st.nextToken());
 				map[i][j]=input;
+				
 				if(input==2) {
-					virus.offer(new Point(i,j));
+					virusList.add(new Point(i,j));
 				}
+				if(input==0) emptySize++;
 			}
 		}
 		
-		for(int i=0;i<N;i++) {
-			for(int j=0;j<M;j++) {
-				if(map[i][j]==0) {
-					map[i][j]=1;
-					dfs(i*M+j,1);
-					map[i][j]=0;
-				}
-			}
-		}
+		emptySize-=3;
+		
+		dfs(0,0);
 		
 		bw.write(answer+"");
 		bw.close();
 	}
 	
-	static int answer=Integer.MIN_VALUE;
+	static int answer=0;
+	
 	static void dfs(int idx, int cnt) {
+		
+		
 		if(cnt==3) {
-			bfs();
-			answer=Math.max(answer, check());
+			int size=bfs();
+			answer=Math.max(answer, size);
+			
 			return;
 		}
 		
-		for(int i=idx+1;i<N*M;i++) {
-			int r=i/M;
-			int c=i%M;
-			if(map[r][c]==0) {
-				map[r][c]=1;
-				dfs(i,cnt+1);
-				map[r][c]=0;
-			}
-		}
+		if(idx==N*M) return;
 		
+		int r=idx/M;
+		int c=idx%M;
+		
+		if(map[r][c]==0) {
+			map[r][c]=1;
+			dfs(idx+1, cnt+1);
+			map[r][c]=0;
+		}
+		dfs(idx+1, cnt);
 	}
 	
-	static void bfs() {
+	static int bfs() {
+		Queue<Point> q=new ArrayDeque<>();
 		v=new boolean[N][M];
-		Queue<Point> q=new ArrayDeque();
-		q.addAll(virus);
+		int size=emptySize;
+		
+		q.addAll(virusList);
+		
 		while(!q.isEmpty()) {
 			Point p=q.poll();
+			
 			for(int d=0;d<4;d++) {
 				int nr=p.r+dr[d];
 				int nc=p.c+dc[d];
 				if(nr>=0&&nr<N&&nc>=0&&nc<M&&map[nr][nc]==0&&!v[nr][nc]) {
 					q.offer(new Point(nr,nc));
 					v[nr][nc]=true;
+					size--;
 				}
 			}
 		}
-	}
-	
-	static int check() {
-		int cnt=0;
 		
-		for(int i=0;i<N;i++) {
-			for(int j=0;j<M;j++) {
-				if(map[i][j]==0&&!v[i][j]) {
-					cnt++;
-				}
-			}
-		}
-		return cnt;
+		return size;
 	}
-
 }
