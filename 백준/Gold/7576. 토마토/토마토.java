7a62c2d7b5ccccedae5aab4d7f97a9cccbb570f7
elle6044 <123047819+elle6044@users.noreply.github.com>
2025-04-20 11:47:06 +0900

    [Gold V] Title: 토마토, Time: 520 ms, Memory: 101560 KB -BaekjoonHub

--- 백준/Gold/7576. 토마토/토마토.java
+++ 백준/Gold/7576. 토마토/토마토.java
@@ -1,9 +1,8 @@
-
 import java.util.*;
 import java.io.*;
-public class Main {
 
-	static BufferedReader br= new BufferedReader(new InputStreamReader(System.in));
+public class Main {
+	static BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
 	static BufferedWriter bw=new BufferedWriter(new OutputStreamWriter(System.out));
 	static StringTokenizer st;
 	
@@ -11,39 +10,27 @@ public class Main {
 	static int[][] map;
 	static boolean[][] v;
 	
-	static Queue<Point> q=new ArrayDeque<>();
+	static int[] dr= {1,-1,0,0};
+	static int[] dc= {0,0,1,-1};
 	
 	public static void main(String[] args) throws Exception{
 		st=new StringTokenizer(br.readLine());
+		
 		M=Integer.parseInt(st.nextToken());
 		N=Integer.parseInt(st.nextToken());
+		
 		map=new int[N][M];
-		v=new boolean[N][M];
 		for(int i=0;i<N;i++) {
 			st=new StringTokenizer(br.readLine());
 			for(int j=0;j<M;j++) {
-				int input=Integer.parseInt(st.nextToken());
-				map[i][j]=input;
-				if(input==1) {
-					q.offer(new Point(i,j));
-					v[i][j]=true;
-				}
+				map[i][j]=Integer.parseInt(st.nextToken());
+				if(map[i][j]==0) cnt++;
 			}
 		}
 		
-		int answer=bfs();
+		bfs();
 		
-		boolean check=true;
-		
-		for(int i=0;i<N;i++) {
-			for(int j=0;j<M;j++) {
-				if(map[i][j]==0) {
-					check=false;
-				}
-			}
-		}
-		
-		if(check) {
+		if(cnt==0) {
 			bw.write(answer+"");
 		}
 		else {
@@ -52,32 +39,47 @@ public class Main {
 		bw.close();
 	}
 	
-	static int[] dr= {1,-1,0,0};
-	static int[] dc= {0,0,1,-1};
-	static int bfs() {
-		int cnt=0;
+	static int cnt=0;
+	static int answer=0;
+	
+	static void bfs() {
+		Queue<Point> q=new ArrayDeque<Point>();
+		v=new boolean[N][M];
+		
+		for(int i=0;i<N;i++) {
+			for(int j=0;j<M;j++) {
+				if(map[i][j]==1) {
+					q.offer(new Point(i,j));
+					v[i][j]=true;
+				}
+			}
+		}
+		
+		
+		
 		while(!q.isEmpty()) {
 			int size=q.size();
-			boolean check=false;
-
+			boolean isChanged=false;
+			
 			for(int s=0;s<size;s++) {
 				Point p=q.poll();
+				
 				for(int d=0;d<4;d++) {
 					int nr=p.r+dr[d];
 					int nc=p.c+dc[d];
+					
 					if(nr>=0&&nr<N&&nc>=0&&nc<M&&map[nr][nc]==0&&!v[nr][nc]) {
 						q.offer(new Point(nr,nc));
 						v[nr][nc]=true;
 						map[nr][nc]=1;
-						check=true;
+						cnt--;
+						isChanged=true;
 					}
 				}
 			}
-			if(check) {
-				cnt++;
-			}
+			
+			if(isChanged) answer++;
 		}
-		return cnt;
 	}
 	
 	static class Point{
