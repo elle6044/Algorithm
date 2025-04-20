56e85446022e1d58bfd8f2a3f30216080e4c4180
elle6044 <123047819+elle6044@users.noreply.github.com>
2025-04-20 13:07:07 +0900

    [Gold III] Title: 벽 부수고 이동하기, Time: 440 ms, Memory: 81032 KB -BaekjoonHub

--- 백준/Gold/2206. 벽 부수고 이동하기/벽 부수고 이동하기.java
+++ 백준/Gold/2206. 벽 부수고 이동하기/벽 부수고 이동하기.java
@@ -14,62 +14,68 @@ public class Main {
 	static int[] dc= {0,0,1,-1};
 	
 	static class Point{
-		int r,c,wall,cost;
-		public Point(int r, int c, int wall, int cost) {
+		int r,c,w,cnt;
+		public Point(int r, int c, int w, int cnt) {
 			this.r=r;
 			this.c=c;
-			this.wall=wall;
-			this.cost=cost;
+			this.w=w;
+			this.cnt=cnt;
 		}
 	}
 	
 	public static void main(String[] args) throws Exception{
 		st=new StringTokenizer(br.readLine());
+		
 		N=Integer.parseInt(st.nextToken());
 		M=Integer.parseInt(st.nextToken());
 		
 		map=new int[N][M];
-		v=new boolean[2][N][M];
-		
 		for(int i=0;i<N;i++) {
 			String input=br.readLine();
 			for(int j=0;j<M;j++) {
-				char word=input.charAt(j);
-				map[i][j]=Character.getNumericValue(word);	
+				int num=Character.getNumericValue(input.charAt(j));
+				map[i][j]=num;
 			}
 		}
 		
 		bfs();
 		
-		bw.write((answer==Integer.MAX_VALUE?-1:answer)+"");
+		if(answer==Integer.MAX_VALUE) answer=-1;
+		
+		bw.write(answer+"");
 		bw.close();
 	}
 	
 	static int answer=Integer.MAX_VALUE;
 	
-	static void bfs() {
-		Queue<Point> q=new ArrayDeque();
+	static void bfs() throws Exception{
+		Queue<Point> q=new ArrayDeque<>();
+		v=new boolean[2][N][M];
+		
 		q.offer(new Point(0,0,0,1));
 		v[0][0][0]=true;
 		
 		while(!q.isEmpty()) {
 			Point p=q.poll();
 			if(p.r==N-1&&p.c==M-1) {
-				answer=Math.min(answer, p.cost);
+				answer=Math.min(answer, p.cnt);
 			}
 			
 			for(int d=0;d<4;d++) {
 				int nr=p.r+dr[d];
 				int nc=p.c+dc[d];
-				if(nr>=0&&nr<N&&nc>=0&&nc<M&&!v[p.wall][nr][nc]) {
+				
+				if(nr>=0&&nr<N&&nc>=0&&nc<M) {
 					if(map[nr][nc]==0) {
-						q.offer(new Point(nr,nc,p.wall,p.cost+1));
-						v[p.wall][nr][nc]=true;
+						if(!v[p.w][nr][nc]) {
+							q.offer(new Point(nr,nc,p.w,p.cnt+1));
+							v[p.w][nr][nc]=true;
+						}
 					}
 					else {
-						if(p.wall==0) {
-							q.offer(new Point(nr,nc,p.wall+1,p.cost+1));
-							v[p.wall+1][nr][nc]=true;
+						if(p.w==0&&!v[p.w+1][nr][nc]) {
+							q.offer(new Point(nr,nc,p.w+1,p.cnt+1));
+							v[p.w+1][nr][nc]=true;
 						}
 					}
 				}
